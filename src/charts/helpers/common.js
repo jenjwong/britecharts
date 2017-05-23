09d3e418b2e60b00d8e8ce96b651da2d59188ed1
Jen J Wong <jenjwong@gmail.com>
2017-05-22 22:59:16 -0700

    calculates percent

--- src/charts/helpers/common.js
+++ src/charts/helpers/common.js
@@ -1,6 +1,8 @@
 define(function(require) {
     'use strict'
 
+    const d3Format = require('d3-format');
+
     /**
      * Checks if a number is an integer of has decimal values
      * @param  {Number}  value Value to check
@@ -10,8 +12,19 @@ define(function(require) {
         return value % 1 === 0;
     }
 
-    return {
-        isInteger
-    };
+    /**
+     * Calculates percentage of value from total
+     * @param  {Number}  value    Value to check
+     * @param  {Number}  total    Sum of values
+     * @param  {Number}  decimals Number of decimals in Percentage https://github.com/d3/d3-format
+     * @return {Number}           Percentage
+     */
+    function calculatePercent(value, total, decimals) {
+        return d3Format.format(decimals)(value / total * 100)
+    }
 
+    return {
+        isInteger,
+        calculatePercent
+    };
 });
